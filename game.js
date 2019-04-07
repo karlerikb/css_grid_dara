c66b8873fce86f4fa7c761c34bd0b2731c98e372
karlerikb <karlerikb@users.noreply.github.com>
2019-04-07 18:36:23 +0300

    basic hints + piece moving functionality within gameboard

--- game.js
+++ game.js
@@ -2,6 +2,10 @@
 
 const root = document.querySelector(":root");
 const gameboardElement = document.querySelector(".gameboard");
+const playerTurnInHints = document.querySelector(".hintsContainer .playerTurn");
+const playerTurnDescription = document.querySelector(".hintsContainer .turnDescription");
+const playerTurnAdditionalDescription = document.querySelector(".hintsContainer .additionalDescription");
+const gamePhaseInHints = document.querySelector(".hintsContainer .gamePhase");
 
 
 // Variables
@@ -10,6 +14,21 @@ let activePlayer;
 let animationInProgress = false;
 const gameboardPieces = [];
 const prohibitedPositions = [];
+const gamePhases = ["first", "second"];
+const gamePhasesInEstLang = ["esimene", "teine"];
+const playerTurnDescriptions = [
+  "Vali oma mängunupp (vajuta mängunupu peale).",
+  "Paiguta mängunupp lauale (vajuta mõne mängulauale tekkinud positsiooni peale).",
+  "Nuppu paigutatakse..."
+];
+const additionalPlayerTurnDescriptions = [
+  "Mängunupu paigutamisel ei tohi tekkida kolmest rida."
+];
+
+
+/* TEMPORARY VARIABLES for Development */
+const gamePiecesForEachPlayer = 12;
+const animationTime = ".5s";
 
 
 // Initializing Game
@@ -17,10 +36,36 @@ const prohibitedPositions = [];
 function initGame() {
   createPlayerPieces();
   switchTurnToPlayer("one");
+  setInitialDescriptions();
 }
 
 initGame();
 
+
+// Descriptions
+
+function setInitialDescriptions() {
+  setPlayerTurnInHints();
+  setPlayerTurnDescriptionState(0);
+}
+
+function setPlayerTurnInHints() {
+  const index = players.findIndex(player => player === activePlayer);
+  const player = playersEstLang[index];
+  playerTurnInHints.textContent = `${player} kord.`;
+}
+
+function setPlayerTurnDescriptionState(index) {
+  playerTurnDescription.textContent = playerTurnDescriptions[index];
+}
+
+function setGamePhaseDescription() {
+  if (gameboardPieces.length < gamePiecesForEachPlayer * 2) {
+    gamePhaseInHints.textContent = `Mängu ${gamePhasesInEstLang[0]} faas.`;
+  } else {
+    gamePhaseInHints.textContent = `Mängu ${gamePhasesInEstLang[1]} faas.`;
+  }
+}
 
 
 // Creating Game Pieces
@@ -35,12 +80,12 @@ function createPlayerPieces() {
 }
 
 function createPlayerPieceElements(piecesContainerElement, playerNumber) {
-  for (let i = 1; i <= 12; i++) {
+  for (let i = 1; i <= gamePiecesForEachPlayer; i++) {
     const playerPieceElement = document.createElement("div");
     playerPieceElement.setAttribute("id", `p${playerNumber}_${i}`);
     playerPieceElement.className = "piece";
     playerPieceElement.textContent = i;
-    playerPieceElement.addEventListener("click", activatePlayerPiece);
+    playerPieceElement.addEventListener("click", activatePlayerPieceForPhaseOne);
     piecesContainerElement.appendChild(playerPieceElement);
   }
 }
@@ -63,6 +108,8 @@ function switchTurnToPlayer(playerNumber) {
   scrollToActivePlayerPiecesContainer(playerNumber);
   activatingCurrentPlayer(playerNumber);
   deactivatingOpponentPlayer(playerNumber);
+  setInitialDescriptions();
+  testGamePhase();
 }
 
 function activatingCurrentPlayer(playerNumber) {
@@ -85,18 +132,54 @@ function scrollToActivePlayerPiecesContainer(playerNumber) {
 }
 
 
+// Testing for Game Phase
+function testGamePhase() {
+  if (gameboardPieces.length === gamePiecesForEachPlayer * 2) startPhaseTwo();
+}
+
+function startPhaseTwo() {
+  setGamePhaseDescription();
+  prepareGameboardPieces();
+}
+
+function prepareGameboardPieces() {
+  const gameboardPieces = Array.from(gameboardElement.children);
+  gameboardPieces.forEach(piece => {
+    piece.addEventListener("click", activatePlayerPieceForPhaseTwo);
+  });
+}
+
+
+function activatePlayerPieceForPhaseTwo(e) {
+  const playerNumberString = upperCaseFirstLetter(activePlayer);
+  const activatablePieceElements = document.querySelectorAll(`.gameboard > .player${playerNumberString}.piece`);
+  const activatablePieces = Array.from(activatablePieceElements);
+  if (activatablePieces.includes(e.target)) {
+    highlightActivatedPiece(2, e.target, activatablePieces);
+  }
+}
+
+
+// function highlightActivatedPieceForPhaseTwo(activePiece) {
+//   activePiece.textContent = activePiece.id;
+//   highlightActivatedPiece(2, activePiece)
+// }
+
+
+
 // Activating Player Piece
 
-function activatePlayerPiece(e) {
+function activatePlayerPieceForPhaseOne(e) {
   const playerNumberString = upperCaseFirstLetter(activePlayer);
   const activatedPlayerPiecesElements = document.querySelector(`.player${playerNumberString}.piecesContainer`).children;
   const activatedPlayerPieces = Array.from(activatedPlayerPiecesElements);
   if (activatedPlayerPieces.includes(e.target) && !animationInProgress) {
-    highlightActivatedPiece(e.target, activatedPlayerPieces);
+    setPlayerTurnDescriptionState(1);
+    highlightActivatedPiece(1, e.target, activatedPlayerPieces);
   }
 }
 
-function highlightActivatedPiece(activePiece, activatedPlayerPieces) {
+function highlightActivatedPiece(phase, activePiece, activatedPlayerPieces) {
   // if the piece that is clicked on already is already active
   if (activePiece.classList.contains("active")) {
     removePlayerPieceActivation(activatedPlayerPieces);
@@ -105,7 +188,7 @@ function highlightActivatedPiece(activePiece, activatedPlayerPieces) {
   } else {
     setPlayerPieceToActive(activePiece);
     setOtherPiecesToInactive(activePiece, activatedPlayerPieces);
-    createGameboardPositions(activePiece, activatedPlayerPieces);
+    createGameboardPositions(phase, activePiece, activatedPlayerPieces);
   }
 }
 
@@ -130,9 +213,10 @@ function setOtherPiecesToInactive(activePiece, activatedPlayerPieces) {
   });
 }
 
-function createGameboardPositions(activePiece, activatedPlayerPieces) {
+function createGameboardPositions(phase, activePiece, activatedPlayerPieces) {
   removeGameboardPositions();
-  createGameboardPositionElements(activePiece, activatedPlayerPieces);
+  if (phase === 1) createGameboardPositionElementsForPhaseOne(activePiece, activatedPlayerPieces);
+  if (phase === 2) createGameboardPositionElementsForPhaseTwo(activePiece, activatedPlayerPieces);
 }
 
 function removeGameboardPositions() {
@@ -146,7 +230,7 @@ function removeGameboardPositionElements(temporaryPositions) {
   });
 }
 
-function createGameboardPositionElements(activePiece, activatedPlayerPieces) {
+function createGameboardPositionElementsForPhaseOne(activePiece, activatedPlayerPieces) {
   for (let row = 1; row <= 5; row++) {
     for (let column = 1; column <= 6; column++) {
       const position = `a${row}${column}`;
@@ -154,6 +238,44 @@ function createGameboardPositionElements(activePiece, activatedPlayerPieces) {
       createTemporaryPositionElement(position, activePiece, activatedPlayerPieces);
     }
   }
+}
+
+function createGameboardPositionElementsForPhaseTwo(activePiece, activatedPlayerPieces) {
+  const piecePosition = window.getComputedStyle(activePiece).gridArea.split("/")[0].trim();
+  createTemporaryPositionElementsForPhaseTwo(piecePosition, activePiece, activatedPlayerPieces);
+}
+
+function createTemporaryPositionElementsForPhaseTwo(piecePosition, activePiece, activatedPlayerPieces) {
+  const pieceRow = +piecePosition[1];
+  const pieceColumn = +piecePosition[2];
+
+  const topPosition = (pieceRow > 1) ? pieceRow - 1 : null;
+  const bottomPosition = (pieceRow < 5) ? pieceRow + 1 : null;
+  const leftPosition = (pieceColumn > 1) ? pieceColumn - 1 : null;
+  const rightPosition = (pieceColumn < 6) ? pieceColumn + 1 : null;
+
+  const topArea = (topPosition) ? `a${topPosition}${pieceColumn}` : null;
+  const bottomArea = (bottomPosition) ? `a${bottomPosition}${pieceColumn}`: null;
+  const leftArea = (leftPosition) ? `a${pieceRow}${leftPosition}` : null;
+  const rightArea = (rightPosition) ? `a${pieceRow}${rightPosition}` : null;
+
+  const areas = [topArea, bottomArea, leftArea, rightArea];
+
+  areas.forEach(area => {
+    if (area && !prohibitedPositions.includes(area)) {
+      createTemporaryPositionElementForPhaseTwo(area, activePiece, activatedPlayerPieces);
+    }
+  });
+}
+
+function createTemporaryPositionElementForPhaseTwo(area, activePiece, activatedPlayerPieces) {
+  const temporaryPositionElement = document.createElement("div");
+  temporaryPositionElement.className = "temporaryPosition";
+  temporaryPositionElement.style.gridArea = area;
+  temporaryPositionElement.addEventListener("click", moveActivePieceToGameboard);
+  temporaryPositionElement.activePiece = activePiece;
+  temporaryPositionElement.activatedPlayerPieces = activatedPlayerPieces;
+  gameboardElement.appendChild(temporaryPositionElement);
 }
 
 function createTemporaryPositionElement(position, activePiece, activatedPlayerPieces) {
@@ -170,7 +292,6 @@ function createTemporaryPositionElement(position, activePiece, activatedPlayerPi
 // Moving Piece from PiecesContainer to Gameboard
 
 function moveActivePieceToGameboard(e) {
-  // const opponentPlayer = (players.filter(player => player !== activePlayer))[0];
   const playerPieceElement = e.target.activePiece;
   const gameboardPositionElement = e.target;
   const activatedPlayerPieces = e.target.activatedPlayerPieces;
@@ -181,9 +302,21 @@ function moveActivePieceToGameboard(e) {
 
 function addPlayerPieceToGameboard(playerPieceId, gameboardPositionElement) {
   const gameboardPositionArea = window.getComputedStyle(gameboardPositionElement).gridArea.split("/")[0].trim();
-  gameboardPieces.push(playerPieceId);
-  prohibitedPositions.push(gameboardPositionArea);
+  configureGameboardPiecesLogic(playerPieceId, gameboardPositionArea);
   createPlayerPieceElement(playerPieceId, gameboardPositionArea);
+}
+
+function configureGameboardPiecesLogic(playerPieceId, gameboardPositionArea) {
+  if (!gameboardPieces.includes(playerPieceId)) {
+    gameboardPieces.push(playerPieceId);
+    prohibitedPositions.push(gameboardPositionArea);
+  } else {
+    const index = gameboardPieces.findIndex(pieceId => pieceId === playerPieceId);
+    prohibitedPositions.splice(index, 1);
+    gameboardPieces.splice(index, 1);
+    gameboardPieces.push(playerPieceId);
+    prohibitedPositions.push(gameboardPositionArea);
+  }
 }
 
 function removePlayerActivePiece(piece) {
@@ -201,6 +334,9 @@ function animateMovementBetweenGrids(activePiece, targetPosition, activatedPlaye
 
   root.style.setProperty("--targetPositionTop", (targetPositionTop - activePieceTop) + "px");
   root.style.setProperty("--targetPositionLeft", (targetPositionLeft - activePieceLeft) + "px");
+
+  /* temporary setting for development */
+  root.style.setProperty("--animationTime", animationTime);
 
   activePiece.classList.add("moveBetweenGrids");
   activePiece.addEventListener("animationend", animationBetweenGridsEnds);
@@ -233,6 +369,7 @@ function animationStarted(e) {
   temporaryPositions.forEach(temporaryPosition => {
     if (!temporaryPosition.classList.contains("notAllowed")) temporaryPosition.classList.add("notAllowed");
   });
+  setPlayerTurnDescriptionState(2);
   animationInProgress = true;
 }
 
